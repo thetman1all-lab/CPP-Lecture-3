5e5ce935003744012dd07a459cb309ebbb69060b
tcounihan1 <thetman1all@gmail.com>
2026-03-06 18:07:12 -0800

    Practicing file input and output

---
+++ File-Practice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+
+
+int main(){
+
+    // Use ifstream to link file to program to be able to extract data from it
+    std::ifstream inputFile("telemetry_sample.txt");
+
+    
+
+    return 0;
+}
