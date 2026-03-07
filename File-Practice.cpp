0a1b827096f4d13daa16de8f51c5109a92830df3
tcounihan1 <thetman1all@gmail.com>
2026-03-06 18:12:20 -0800

    Should now open a file and print to terminal each line

--- File-Practice.cpp
+++ File-Practice.cpp
@@ -12,7 +12,22 @@ int main(){
     // Use ifstream to link file to program to be able to extract data from it
     std::ifstream inputFile("telemetry_sample.txt");
 
-    
+    // Validate the opening of the file
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Failed to open file!" << std::endl;
+        return 1;  // Exit with error code
+    }
+
+    // Initialize variable for lines
+    std::string line;
+
+    // Read entire line with while loop (delimited by newline)
+    while (std::getline(inputFile, line)) {
+        std::cout << "Read line: " << line << std::endl;
+    }
+
+    // Always close the file for good practice
+    inputFile.close();
 
     return 0;
 }
